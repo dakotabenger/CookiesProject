2633ef4d639be8218443e5a9b80dd6ee44d1caa5
Ranson Knorr <ransonknorr@gmail.com>
2020-08-03 15:16:53 -0500

    phase 3 finished

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,8 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("This runs after the DOM has loaded")  
+  console.log("This runs after the DOM has loaded")
 });
 
 window.onload = () => {
   console.log("This script loaded when all the resources and DOM are ready")
 }
 
+document.cookie = 'monster_name=cookie';
+document.cookie = 'favorite_cookie=snickerdoodle';
+document.cookie = 'favorite_icecream=chocolate';
+
+window.alert(document.cookie)
