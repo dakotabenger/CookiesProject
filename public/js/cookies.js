f2c5ec5b5451d6b0a361a9b76493ecc4ced7601b
root <root@DESKTOP-LE7491G.localdomain>
2020-08-03 13:09:30 -0700

    Second Commit

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,4 +1,8 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Your JavaScript goes here...
-  
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("This runs after the DOM has loaded")  
 });
+
+window.onload = () => {
+  console.log("This script loaded when all the resources and DOM are ready")
+}
+
