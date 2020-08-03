9fa4cb363d39cad0a05c0037d1a5f730a10b64b5
root <root@DESKTOP-LE7491G.localdomain>
2020-08-03 13:46:43 -0700

    getCookieValue Function

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -6,8 +6,38 @@ window.onload = () => {
   console.log("This script loaded when all the resources and DOM are ready")
 }
 
-document.cookie = 'monster_name=cookie';
-document.cookie = 'favorite_cookie=snickerdoodle';
-document.cookie = 'favorite_icecream=chocolate';
+document.cookie = "monster_name=cookie"
+document.cookie = 'favorite_cookie=snickerdoodle'
+document.cookie = 'favorite_icecream=chocolate'
 
-window.alert(document.cookie)
+
+const setCookie = function(name,value) {
+  document.cookie = `${name}=${value}`
+}
+
+setCookie("favoriteFood","macandcheese")
+
+const getCookies = () => {
+  return document.cookie.split(" ");
+}
+
+console.log(getCookies());
+
+const getCookieValue = function(name) {
+  let cookieArr = document.cookie.split("=").join(" ").split(";").join(" ").split(" ")
+  
+    console.log(cookieArr);
+    for (i = 0; i < cookieArr.length; i += 3 {
+      let key = cookieArr[i]
+      if (key.includes(name)) {
+        return cookieArr[i + 1];
+      }
+    }
+    return null;
+  }
+
+console.log(getCookieValue("monster_name"));
+
+console.log(getCookieValue("favorite_cookie"));
+
+console.log(getCookieValue("notAKey"));
